1e3d4563d204e2abb3c6ab9ffa9495bffff1e21d
Shinsuke KUROKI <s-kuroki@aiming-inc.com>
2015-09-10 21:11:27 +0900

    Add provisioning to install softwares by homebrew

--- install.sh
+++ install.sh
@@ -6,3 +6,12 @@ brew update
 
 brew install python
 brew install ansible
+brew install git
+
+git clone https://github.com/skuroki/dotfiles2.git
+
+echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.bash_profile
+source ~/.bash_profile
+
+cd dotfiles2/provisioning
+ansible-playbook -i hosts -vv localhost.yml
